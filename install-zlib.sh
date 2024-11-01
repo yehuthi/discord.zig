9d1801296a1b937b1133e9f142ad13b8df71bcd8
rainfall <rain@Lenovo-IdeaPad-S145-14IIL-fe9d1024.localdomain>
2024-10-31 23:42:52 -0500

    fix: build scripts

--- install-zlib.sh
+++ install-zlib.sh
@@ -2,3 +2,4 @@ wget https://github.com/madler/zlib/releases/download/v1.2.13/zlib-1.2.13.tar.gz
 tar xvf zlib-1.2.13.tar.gz
 rm zlib-1.2.13.tar.gz
 mv zlib-1.2.13 zlib
+mv zlib lib/zlib
---
+++ install.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/yuzudev/websocket.zig.git ./lib/websocket.zig/
+git clone https://github.com/yuzudev/zig-tls12 ./lib/zig-tls12/
