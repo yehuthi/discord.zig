41f65d668cd88399a5b068ea813cfc5c1516329e
rainfall <rain@Lenovo-IdeaPad-S145-14IIL-fe9d1024.localdomain>
2024-11-02 19:00:50 -0500

    add new lib to shell script

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/yuzudev/websocket.zig.git ./lib/websocket.zig/
 git clone https://github.com/yuzudev/zig-tls12 ./lib/zig-tls12/
+git clone https://github.com/jetzig-framework/zmpl.git ./lib/zmpl/
+./install-zlib.sh
