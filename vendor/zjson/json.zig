f1f723d53139085fa638c440316431d576e41f99
Yuzu <yuzu@noreply.codeberg.org>
2024-12-07 03:43:57 -0500

    license the whole code

--- vendor/zjson/json.zig
+++ vendor/zjson/json.zig
@@ -1,3 +1,19 @@
+//! ISC License
+//!
+//! Copyright (c) 2024-2025 Yuzu
+//!
+//! Permission to use, copy, modify, and/or distribute this software for any
+//! purpose with or without fee is hereby granted, provided that the above
+//! copyright notice and this permission notice appear in all copies.
+//!
+//! THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+//! REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+//! AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+//! INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+//! LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+//! OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+//! PERFORMANCE OF THIS SOFTWARE.
+//! ---------------------------------------------------------------------------------------------------------------------
 //! JSON Parser
 //! This is an implementation of a JSON parser written using the Zig standard library.
 //! It uses monadic combinators to build an expressive PEG grammar that is extensible to other formats.
